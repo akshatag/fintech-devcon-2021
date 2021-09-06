1f58ee4343282e22e8a2707495084c55785e43b7
Akshat Agrawal <akshat@Akshats-MacBook-Pro.local>
2021-09-06 15:17:53 -0700

    front end styles

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,35 @@
 var express = require('express');
-const { renameSync } = require('fs');
 var router = express.Router();
 const path = require('path')
-var Vault = require(path.join(__dirname + '/../vault.js'))
 
-/* GET home page. */
+var Vault = require(path.join(__dirname + '/../vault.js'))
+const vault = new Vault();
+
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname + '/../public/insert.html'));
+  res.sendFile(path.join(__dirname + '/../views/insert.html'));
 });
 
+router.post('/insert', async function(req, res, next) {
+  var cardHolder = req.body.cardHolder;
+  var cardNumber = req.body.cardNumber;
+  var expiryDate = req.body.expiryDate;
+  var cvv = req.body.cvv
+
+  if(cardHolder.length == 0 || expiryDate.length == 0 || cvv.length == 0) {
+    res.render('formError', {message: "Form error: One or more fields were empty"})
+  } else if(cardNumber.length != 16) {
+    res.render('formError', {message: "Form error: Card number must be 16 digits"})
+  } else {
+    await vault.insert(cardHolder, cardNumber, expiryDate)
+    res.redirect('/all')
+  }
+})
+
 router.get('/all', async function(req, res, next) {
-  var vault = new Vault();
   cards = await vault.get();
-  // console.log(cards)
   res.render('table', {cards: cards})
 })
+
 
 router.get('/detokenize/:token', async function(req, res, next) {
   var vault = new Vault();
@@ -22,14 +37,6 @@ router.get('/detokenize/:token', async function(req, res, next) {
   res.json({value: value})
 })
 
-router.post('/insert', async function(req, res, next) {
-  var vault = new Vault();
-  var cardHolder = req.body.cardholder;
-  var cardNumber = req.body.cardnumber;
-  var expDate = req.body.expdate;
-  console.log('params ' + cardHolder + ' ' + cardNumber)
-  await vault.insert(cardHolder, cardNumber, expDate)
-  res.json(true)
-})
+
 
 module.exports = router;
--- vault.js
+++ vault.js
@@ -6,11 +6,19 @@ Vault.prototype.insert = async function (cardHolder, cardNumber, expDate) {
   /* TODO */
 }
 
-Vault.prototype.get = async function() {
+Vault.prototype.tokenizeCardHolder = function(cardHolder) {
+  /* TODO */
+}
+
+Vault.prototype.tokenizeCardNumber = function(cardNumber) {
   /* TODO */
 }
 
 Vault.prototype.detokenize = async function(token) {
+  /* TODO */
+}
+
+Vault.prototype.get = async function() {
   /* TODO */
 }
 
